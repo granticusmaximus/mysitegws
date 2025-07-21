375c1dad035f973a09107dfd6f6dad62f61434a2
Grant Watson <grant@gwsapp.net>
2025-07-21 12:34:28 -0400

    Minor vite changes and sitemap update

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        navigateFallback: '/offline.html',
+        //navigateFallback: '/offline.html',
         navigateFallbackDenylist: [/\.pdf$/],
         runtimeCaching: [
           {
